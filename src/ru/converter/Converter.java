9e7ef5804aca04db0b104e201f29da1e776b2ee6
Nicolay Golovnev <kolya.golovnev@mail.ru>
2022-04-01 17:08:35 +0700

    Создано начальное тестирование класса

---
+++ src/ru/converter/Converter.java
@@ -0,0 +1,15 @@
+package ru.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Converter {
+
+    public List<String> getSystems() {
+        return new ArrayList<>();
+    }
+
+    public List<String> getUnits() {
+        return new ArrayList<>();
+    }
+}
---
+++ src/ru/converter/TestConverter.java
@@ -0,0 +1,23 @@
+package ru.converter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestConverter {
+    private Converter converter = new Converter();
+
+    @Test
+    public void converterCreationTest() {
+        assertNotNull(converter);
+    }
+
+    @Test
+    public void converterCheckAllSystems() {
+        assertEquals(3, converter.getSystems().size());
+    }
+
+    @Test
+    public void converterCheckAllUnits() {
+        assertEquals(3, converter.getUnits().size());
+    }
+}
